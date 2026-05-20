1e6980c859c0b937eacceb24906bf733d4993fd5
Pdbjork <pdbjork@gmail.com>
2026-05-20 12:44:43 -0500

    Update deprecated NavigationView to NavigationStack

--- Route66App/ContentView.swift
+++ Route66App/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
+        NavigationStack {
             Route66MapView()
                 .navigationTitle("Route 66")
         }
